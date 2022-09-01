1bfc214dc6129ae72a242994da092ef85763ee5e
Smoren <ofigate@gmail.com>
2022-09-01 22:45:41 +0300

    fix

---
+++ tests/unit/MainTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Smoren\QueryRelationManager\Tests\Unit;
+
+class MainTest extends \Codeception\Test\Unit
+{
+    public function testFirst()
+    {
+        // TODO make tests
+    }
+}
