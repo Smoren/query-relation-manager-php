01210e9dfc5b8e4bc633add1f721d222f9a6d305
Smoren <ofigate@gmail.com>
2022-09-02 10:07:01 +0300

    connect

--- tests/scripts/migrate.php
+++ tests/scripts/migrate.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "Under construction...";
+try {
+    $dbh = new PDO('mysql:host=mysql;dbname=test', 'root');
+    echo "Connection success";
+} catch(PDOException $e) {
+    echo "Cannot connect";
+}
